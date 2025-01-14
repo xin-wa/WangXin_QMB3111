5a5f3c3ddbb4de57db1fba7fcf0dc6114a97784d
xin-wa <xi429252@ucf.edu>
2025-01-13 23:58:29 -0500

    Update Assignment_01.py

--- Assignment_01/Assignment_01.py
+++ Assignment_01/Assignment_01.py
@@ -1,10 +1,16 @@
+from datetime import datetime
+current_year = datetime.now().year
+
+current_year %= 2000
+birth_year = 2000
+
 number = 4
 
 number *= 2
 number += 5
 number *= 50
 number += 1749
-number += 25
-number -= 2000
+number += current_year
+number -= birth_year
 
 print (number)
